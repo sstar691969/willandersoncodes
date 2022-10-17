0af0628753f9d6df224c177ffa6ba33736eda889
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-10-17 10:50:48 -0700

    Add files via upload

---
+++ it261/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    
+    
+
+<header>
+
+<div class="inner-header">
+
+
+
+<nav>
+<ul>
+    
+    <li><a href="">Using a Switch, HW3</a></li>
+    <li><a href="">Troubleshoot</a></li>
+    <li><a href="">Travel Calculator</a></li>
+    <li><a href="">Email</a></li>
+    <li><a href="">Gallery</a></li>
+    <li><a href="">Database</a></li>
+
+</ul>
+    
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+    
+<!--"end inner header"-->
+</header>
+
+<div id="wrapper">
+
+<div id="hero">
+
+    <a href="index.html">
+
+        <img id="Mamp screenshot" src="images/me.jpg" alt="mamp photo"/>
+        
+        
+        </a>
+        
+<!-----scr image could go here-->
+</div>
+<!-- end hero -->
+
+<main>
+<h1>William Anderson's Portal Page</h1>
+
+<p>Hello, Im William and I love traveling and walking along Seward park and Alki. My goal is to complete my web development certificate by 2023.
+
+
+
+
+</p>
+
+
+</main>
+
+<aside>
+<h3>My Class Assignments: Week 2 thru Week 8</h3>
+
+<ul>
+        <li>Week 2</li>
+        <li><a href="weeks/week2/var.php">var.php</a></li>
+        <li><a href="../index.php">var2.php</a></li>
+        <li><a href="../index.php">currency-logic.php</a></li>
+        <li><a href="../index.php">currency.php</a></li>
+        <li><a href="../index.php">heredoc.php</a></li>
+        
+
+        <li>Week 3</li>
+        <li><a href="../index.php">if.php</a></li>
+        <li><a href="../index.php">date.php</a></li>
+        <li><a href="../index.php">forloop.php</a></li>
+        <li><a href="../index.php">foreach.php</a></li>
+        <li><a href="../index.php">switch.php</a></li>
+
+        <li>Week 4</li>
+        <li><a href="../index.php">form.get.php</a></li>
+        <li><a href="../index.php">form1.php</a></li>
+        <li><a href="../index.php">form2.php</a></li>
+        <li><a href="../index.php">form3.php</a></li>
+        <li><a href="../index.php">form-arithmetic.php</a></li>
+        <li><a href="../index.php">celcius.php</a></li>
+
+        <li>Week 5</li>
+        <li><a href="../index.php">currency1.php></a></li>
+        <li><a href="../index.php">currency2.php></a></li>
+        <li><a href="../index.php">currency3.php></a></li>
+        <li><a href="../index.php">currency4.php></a></li>
+        <li><a href="../index.php">currency5.php></a></li>
+        
+        
+        <li>Week 6</li>
+        <li><a href="../index.php">var.php</a></li>
+        <li><a href="../index.php">implode.php</a></li>
+        <li><a href="../index.php">functions.php</a></li>
+        
+        <li>Week 7</li>
+        <li><a href="../index.php">random.php</a></li>
+        <li><a href="../index.php">pictures.php</a></li>
+        <li><a href="../index.php">strings.php</a></li>
+        
+
+        <li>Week 8</li>
+        <li><a href="../index.php">people.php</a></li>
+        
+
+        
+    </ul>
+
+
+
+</aside>
+
+<!--mamp screen & error_php screenshot goes here-->
+
+<p>mamp screenshot</p>
+    <a href="index.html">
+
+    <img id="Mamp screenshot" src="images/mamp.jpeg" alt="mamp photo"/>
+    
+    
+    </a>
+    
+<p>error php screenshot</p>
+    <a href="index.html">
+
+        <img id="error php screenshot" src="images/error_php.jpeg" alt="error photo"/>
+
+        
+        
+        </a>
+
+</div>
+<!-- end wrapper -->
+<footer>
+    <ul>
+        <li>Copyright &copy;
+            2022</li>
+        <li>All Rights Reserved</li>
+        <li><a href="../index.php">Web Design by William L Anderson</a></li>
+        <li><a id="html-checker" href="#">HTML Validation</a></li>
+        <li><a id="css-checker" href="#">CSS Validation</a></li>
+    </ul>
+
+<script>
+        document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+        document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+</script>
+        
+
+
+</footer>
+
+</body>
+</html>
